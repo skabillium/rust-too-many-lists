f9fab2883bfc2b59d62e78db612e230033590dcd
Bill Kyriazopoulos <skabillium@gmail.com>
2024-12-27 12:04:55 +0200

    Rename files for clarity

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-pub mod fifth;
-pub mod first;
-pub mod fourth;
+pub mod bad_safe_deque;
+pub mod bad_stack;
+pub mod ok_stack;
+pub mod ok_unsafe_queue;
+pub mod persistent_stack;
 pub mod production_unsafe_deque;
-pub mod second;
-pub mod third;
